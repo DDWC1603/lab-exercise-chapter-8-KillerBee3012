efcc01bb4c65e3db2917ef04fd9aa849d5af845c
KillerBee3012 <33883984+KillerBee3012@users.noreply.github.com>
2018-01-30 10:07:12 +0800

    Find 'Segi Tiga' Done Updated

---
+++ class 3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+class CariTigaSegi
+
+{
+	
+	private:
+		int x,y;
+		
+	public:
+		
+		
+		void set_value(int, int );
+		
+		int area()
+		{
+			return (x*y)/2;
+			
+		}
+	
+};
+
+void CariTigaSegi::set_value(int a ,int b)
+{
+	x = a;
+	y = b;
+	
+	
+}
+
+
+
+int main()
+
+{
+	CariTigaSegi hahaha;
+	hahaha.set_value(10,9);
+	hahaha.area();
+	cout<<hahaha.area();
+	
+	
+	
+	
+}
+
